411180dfb06e2a2a22529b77963f8a05953e85b6
nicholas <bul-nick-al@ya.ru>
2018-05-31 11:44:21 +0300

    deleted inappropriate midi files

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import os
 from neat import *
+from neat.checkpoint import Checkpointer
+from neat.config import Config
+from neat.genome import DefaultGenome
 from neat.nn import RecurrentNetwork
+from neat.population import Population
+from neat.reporting import StdOutReporter
+from neat.reproduction import DefaultReproduction
+from neat.species import DefaultSpeciesSet
+from neat.stagnation import DefaultStagnation
+from neat.statistics import StatisticsReporter
 
 from fitness import *
+
 
 def main():
     conf_path = os.path.join(os.path.dirname(__file__), 'neat-config')
@@ -18,14 +28,17 @@ def main():
     winner = p.run(eval_genomes, 300)
     print(winner)
 
+
 def build_generator_function(genome, config: Config):
     nn = RecurrentNetwork.create(genome, config)
     return lambda input: nn.activate(input)
+
 
 def eval_genomes(genomes, config):
     for genome_id, genome in genomes:
         func = build_generator_function(genome, config)
         genome.fitness = eval_function(func)
+
 
 if __name__ == "__main__":
     main()
