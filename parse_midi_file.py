e27ae3a0a066757a644141b0e70b712a33850a1b
Nastya <anastasia.vorton@gmail.com>
2018-05-31 11:39:40 +0300

    Converting each note to (0, 12) scale and writing it into the form suitable for nn input (list of doubles, either 0.0 or 1.0)

--- parse_midi_file.py
+++ parse_midi_file.py
@@ -9,14 +9,20 @@ class MidiParser:
         self.output = []
         mid_file = MidiFile(file_path)  # MidiFile object creation specifying the path
         for i, track in enumerate(mid_file.tracks):  # for each track in a file
-            # print('Track {}: {}'.format(i, track.name))miot3o
+            # print('Track {}: {}'.format(i, track.name))
+            i = 0
             for msg in track:  # print each message in a track
                 if not msg.is_meta and hasattr(msg, 'note') and msg.type == 'note_on':
+                    i += 1
                     note = msg.note
-                    # print(note)
+                    print(note)
                     note = note % 12
                     print(note)
-                    self.output.append(note)
+                    nn_input = [0.0 for x in range(12)]
+                    nn_input[note] = 1.0
+                    print(nn_input)
+                    self.output.append(nn_input)
+            print("number of notes = ", i)
 
 
 outpt = MidiParser('dataset/prima.mid')
