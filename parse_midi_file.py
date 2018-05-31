26746589c49c58a448b3ca1297d084f24abe21ba
Nastya <anastasia.vorton@gmail.com>
2018-05-31 11:22:26 +0300

    Rewritten as a class

--- parse_midi_file.py
+++ parse_midi_file.py
@@ -1,8 +1,23 @@
 from mido import MidiFile  # library imports
 
-mid_file = MidiFile('dataset/away.mid')  # MidiFile object creation specifying the path
-for i, track in enumerate(mid_file.tracks):  # for each track in a file
-    # print('Track {}: {}'.format(i, track.name))miot3o
-    for msg in track:  # print each message in a track
-        if not msg.is_meta and hasattr(msg, 'note') and msg.type == 'note_on':
-            print(msg.note)
+
+class MidiParser:
+    def __init__(self, file_path):
+        self.path = file_path
+
+        #  Parse the file and store the data for the neural network.
+        self.output = []
+        mid_file = MidiFile(file_path)  # MidiFile object creation specifying the path
+        for i, track in enumerate(mid_file.tracks):  # for each track in a file
+            # print('Track {}: {}'.format(i, track.name))miot3o
+            for msg in track:  # print each message in a track
+                if not msg.is_meta and hasattr(msg, 'note') and msg.type == 'note_on':
+                    note = msg.note
+                    # print(note)
+                    note = note % 12
+                    print(note)
+                    self.output.append(note)
+
+
+outpt = MidiParser('dataset/prima.mid')
+print(outpt.output)
