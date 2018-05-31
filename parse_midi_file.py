f102b0270df52e8cb018f72d10b87e892ccce727
nicholas <bul-nick-al@ya.ru>
2018-05-31 09:53:34 +0300

    one comment added

--- parse_midi_file.py
+++ parse_midi_file.py
@@ -2,7 +2,7 @@ from mido import MidiFile  # library imports
 
 mid_file = MidiFile('dataset/away.mid')  # MidiFile object creation specifying the path
 for i, track in enumerate(mid_file.tracks):  # for each track in a file
-    # print('Track {}: {}'.format(i, track.name))
+    # print('Track {}: {}'.format(i, track.name))miot3o
     for msg in track:  # print each message in a track
         if not msg.is_meta and hasattr(msg, 'note') and msg.type == 'note_on':
             print(msg.note)
