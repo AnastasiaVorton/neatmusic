16d9a350e5a1e5111e97b16e44d7c39a945dd5bc
nicholas <bul-nick-al@ya.ru>
2018-06-03 18:01:10 +0300

    the parser initializer now accepts two more parameters: number of octaves and number of ticks per bit

--- parse_midi_file.py
+++ parse_midi_file.py
@@ -2,20 +2,21 @@ from mido import MidiFile  # library imports
 
 
 class MidiParser:
-    def __init__(self, file_path):
+    def __init__(self, file_path, number_of_octaves=1, ticks_per_bit=4):
         self.path = file_path
+        self.number_of_octaves = number_of_octaves
 
         #  Parse the file and store the data for the neural network.
         self.output = []
         mid_file = MidiFile(file_path)  # MidiFile object creation specifying the path
-        ticks_per_atomic_duration = mid_file.ticks_per_beat / 4
+        ticks_per_atomic_duration = mid_file.ticks_per_beat / ticks_per_bit
         track = mid_file.tracks[0]  # there's only one track in the input midi
         self.track_to_nn_inputs(track, ticks_per_atomic_duration)
 
     def track_to_nn_inputs(self, track, ticks_per_atomic_duration):
         for msg in track:  # print each message in a track
             if not msg.is_meta and hasattr(msg, 'note') and msg.type == 'note_on' and not msg.time == 0:
-                nn_input = [0.0 for _ in range(12)]
+                nn_input = [0.0 for _ in range(12 * self.number_of_octaves)]
 
                 ticks_in_note = msg.time / ticks_per_atomic_duration
                 print(ticks_in_note, nn_input)
@@ -25,8 +26,8 @@ class MidiParser:
 
             if not msg.is_meta and hasattr(msg, 'note') and msg.type == 'note_off':
                 note = msg.note
-                note = note % 12
-                nn_input = [0.0 for _ in range(12)]
+                note = note % 12 * self.number_of_octaves
+                nn_input = [0.0 for _ in range(12 * self.number_of_octaves)]
                 nn_input[note] = 1.0
 
                 ticks_in_note = msg.time / ticks_per_atomic_duration
@@ -36,5 +37,5 @@ class MidiParser:
                         self.output.append(nn_input)
 
 
-output = MidiParser('dataset/X Files.mid')
+output = MidiParser('dataset/X Files.mid', 2, 8)
 print(output.output)
