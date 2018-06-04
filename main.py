d41cbe1003aa7a7da671dd2d964eada087636a3b
Nikita Vilunov <nikitaoryol@gmail.com>
2018-06-04 10:57:48 +0300

    Examples of library usage

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from neat import *
 from neat.checkpoint import Checkpointer
 from neat.config import Config
 from neat.genome import DefaultGenome
-from neat.nn import RecurrentNetwork
+from neat.nn import MLRecurrentNetwork
 from neat.population import Population
 from neat.reporting import StdOutReporter
 from neat.reproduction import DefaultReproduction
@@ -23,21 +23,23 @@ def main():
     p.add_reporter(StdOutReporter(True))
     stats = StatisticsReporter()
     p.add_reporter(stats)
-    p.add_reporter(Checkpointer(5))
+    p.add_reporter(Checkpointer(50))
 
-    winner = p.run(eval_genomes, 300)
+    winner = p.run(eval_genomes, 10000)
     print(winner)
+    for test in valid_tests:
+        net = build_generator_function(winner, config)
+        print(eval_function(net, test))
 
 
 def build_generator_function(genome, config: Config):
-    nn = RecurrentNetwork.create(genome, config)
-    return lambda input: nn.activate(input)
+    return MLRecurrentNetwork.create(genome, config)
 
 
 def eval_genomes(genomes, config):
     for genome_id, genome in genomes:
         func = build_generator_function(genome, config)
-        genome.fitness = eval_function(func)
+        genome.fitness = eval_tests(func)
 
 
 if __name__ == "__main__":
